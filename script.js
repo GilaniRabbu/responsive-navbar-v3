b1375457f121c2ba5e938088bc6d2e8ccc03213e
Toufiq Gilani Rabbu <113186897+GilaniRabbu@users.noreply.github.com>
2023-11-20 13:16:05 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const menuOpen = document.querySelector(".menu");
+const menuClose = document.querySelector(".close");
+const overlay = document.querySelector(".overlay");
+const links = document.querySelectorAll('.overlay-link');
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+links.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    const target = document.querySelector(link.hash);
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    menuClose.click(); // close menu
+  });
+});
